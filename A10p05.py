162980008c0e55a264bfd38b3c8c3c6ce1b8d757
Helio Rocha <heliorocha@about.me>
2015-02-22 11:03:44 -0300

    Update A10p05.py

--- A10p05.py
+++ A10p05.py
@@ -1,9 +1,9 @@
 '''
-Faça um programa que recebe do usuário os 
+FaÃ§a um programa que recebe do usuÃ¡rio os 
 elementos de duas listas com 10 elementos 
 cada. O seu programa deve somar as duas 
 listas e mostrar a lista resultante. A soma de 
-listas é a soma das posições correspondentes'''
+listas Ã© a soma das posiÃ§Ãµes correspondentes'''
 
 l1 = []
 l2 = []
@@ -13,4 +13,4 @@ for i in range(10):
     l2.append(input('elemento da lista 2: '))
     l3.append(l1[i]+l2[i])
     
-l4 = [l1[i] + l2[i] for i in range(len(l1))]
+#l4 = [l1[i] + l2[i] for i in range(len(l1))]
